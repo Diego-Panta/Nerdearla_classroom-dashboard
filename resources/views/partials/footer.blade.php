821c1a7c3b3d78d758466c62e0b7c739b139a8c8
Nilton Ramos <niltonencarnacion17@gmail.com>
2025-09-25 18:36:05 -0500

    feat: agregar footer

--- resources/views/partials/footer.blade.php
+++ resources/views/partials/footer.blade.php
@@ -1,1 +1,13 @@
-<h1>Footer</h1>
+<footer class="bg-theme-seasalt backdrop-blur-md w-full flex flex-col items-center justify-center py-6 shadow-md font-bold">
+  <div class="text-sm flex items-center gap-1">
+    © {{ now()->year }}
+    <a
+      class="text-current ml-1 text-theme-xanthous font-extrabold"
+      href="https://www.linkedin.com/in/nilton-ramos-encarnacion-0819b433b/"
+      target="_blank">
+      <span class="hidden sm:block">Code Trinity</span>
+      <span class="block sm:hidden">Trinity</span>
+    </a>
+    <span class="ml-1">Todos los derechos reservados.</span>
+  </div>
+</footer>
