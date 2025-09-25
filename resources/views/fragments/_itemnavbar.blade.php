03bd96134c4b58ed0c7173639a76b77823899157
Nilton Ramos <niltonencarnacion17@gmail.com>
2025-09-25 18:36:43 -0500

    feat(fragments): agregar itemnavbar

---
+++ resources/views/fragments/_itemnavbar.blade.php
@@ -0,0 +1,7 @@
+<li>
+    <a href="" class="block text-theme-onyx hover:text-theme-xanthous transition-colors font-bold">Propuestas</a>
+</li>
+
+<li>
+    <a href="" class="block text-theme-onyx hover:text-theme-xanthous transition-colors font-bold">Nosotros</a>
+</li>
